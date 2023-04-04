76c857134deacc9bcbdce2730803f35fb05f245e
UBnkantah <90422770+UBnkantah@users.noreply.github.com>
2023-04-04 09:37:17 +0100

    added fyto-app & gymate project

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import GitSearch from "../Images/GithubAPI.png"
 import QuizApp from "../Images/QuizApp.png"
-// import Timer from "../Images/SetTime.png"
+import gymImg from "../Images/hero-img-mob.png"
+import fytoImg from "../Images/blog-third-img.png"
+import Timer from "../Images/setTime.png"
 import manaPg from "../Images/mana.png"
 import {motion, Variants} from "framer-motion";
 import "./Project.css"
@@ -31,6 +33,35 @@ function Project() {
         viewport={{once:false, amount:0.5}}
         transition={{staggerChildren:0.5}}
         >
+            <motion.div className='sub_project'
+            variants={imgAnimate}
+            whileHover={{scale: 1.1}}>
+                <div className="project-img">
+                    <img src={gymImg} alt="" />
+                </div>
+                <hr />
+                <div className="view">
+                    <h3>Gymate Platform</h3>
+                    <button>
+                        <a href="https://gymate-psi.vercel.app/" target="_blank">VIEW</a>
+                    </button>
+                </div>
+            </motion.div>
+
+            <motion.div className='sub_project'
+            variants={imgAnimate}
+            whileHover={{scale: 1.1}}>
+                <div className="project-img">
+                    <img src={fytoImg} alt="" />
+                </div>
+                <hr />
+                <div className="view">
+                    <h3>Toka Nft Platform</h3>
+                    <button>
+                        <a href="https://fyto-app.vercel.app/" target="_blank">VIEW</a>
+                    </button>
+                </div>
+            </motion.div>
             
             <motion.div className='sub_project'
             variants={imgAnimate}
@@ -46,7 +77,7 @@ function Project() {
                     </button>
                 </div>
             </motion.div>
-            {/* <motion.div className='sub_project' whileHover={{scale: 1.1}}>
+            <motion.div className='sub_project' whileHover={{scale: 1.1}}>
                 <div className="project-img">
                     <img src={Timer} alt="" />
                 </div>
@@ -57,7 +88,7 @@ function Project() {
                         <a href="https://quiet-licorice-b541f7.netlify.app/" target="_blank">VIEW</a>
                     </button>
                 </div>
-            </motion.div> */}
+            </motion.div>
             <motion.div 
             variants={imgAnimate}
             className='sub_project' 
